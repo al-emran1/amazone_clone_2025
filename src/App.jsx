2db96dd7517a79f97cfceaa33f784f4b42ed4e87
al-emran1 <nasseremranali@gmail.com>
2025-09-24 16:13:26 -0400

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Header from './assets/Components/Header/Header'
+import Carousel from './assets/Components/carousel/Carouselimg'
 function App() {
 
   return (
     <div>
       <Header/>
+      <Carousel/>
     </div>
   )
 }
